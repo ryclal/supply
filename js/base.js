4c34ff7a1a7ff31d5c14b13130165ab795eed450
george <georgedavids96@outlook.com>
2016-08-21 03:33:26 +0300

    feat: create pages for women,men ,kids, boutiques, look book, start boutique description page

--- js/base.js
+++ js/base.js
@@ -1,3 +1,4 @@
 // @codekit-append "../libs/jquery/jquery.min.js";
+// @codekit-append "../libs/jquery/jquery_ui.js";
 // @codekit-append "../libs/bootstrap/js/bootstrap.min.js";
 // @codekit-append "custom.js";
--- js/custom.js
+++ js/custom.js
@@ -20,12 +20,68 @@ $(document).ready(function () {
     toggleNavbarMethod();
     $(window).resize(toggleNavbarMethod);
 
-    $('.modal').on('hidden.bs.modal', function(){
+    $('.modal').on('hidden.bs.modal', function () {
         $(this).find('form')[0].reset();
     });
 
+    $('*').click(function (event) {
+        if (!$(event.target).closest('.search_b').length) {
+            $('.search_b').removeClass('expand');
+        }
+    });
 
-    //Modal open
+    // Split view
+    $('.split_view_block .split').unbind().on('click', function () {
+        var prev_element = $(this),
+            prev_value = prev_element.data('open');
+
+        if (!prev_element.hasClass('active')) {
+            var active_element = $('.split_view_block .split.active'),
+                active_value = active_element.data('open');
+
+            active_element.removeClass('active');
+            prev_element.addClass('active');
+
+            $('.product_side ' + '.' + active_value).removeClass('hidden');
+            $('.product_side ' + '.' + prev_value).addClass('hidden');
+        }
+        return false;
+    });
+
+
+    //Colors checkbox
+
+    $(".color_list label").click(function () {
+
+        $(".color_list").addClass('darker');
+        $(this).toggleClass('active');
+
+    });
+
+    $('.search_trigger').unbind().on('click', function () {
+        $(this).parents('#search_b').toggleClass('expand');
+    });
+
+
+    var slider_b = $('#slider');
+    slider_b.slider({
+        range: true,
+        min: 0,
+        max: 1000,
+        values: [75, 300],
+        slide: function (event, ui) {
+            $(".slider_block .first_value").text("$" + ui.values[0]);
+            $(".slider_block .second_value").text("$" + ui.values[1]);
+        }
+    });
+    $('.slider_block .first_value').text("$" + slider_b.slider("values", 0));
+    $('.slider_block .second_value').text("$" + slider_b.slider("values", 1));
+
+
+    $("#filter_select,#filter_select_1").selectmenu();
+
+
+//Modal open
     $('.open_modal').on('click', function () {
         var to = $(this).data('modal');
 
@@ -45,29 +101,28 @@ $(document).ready(function () {
             $('#register_mail_Modal').modal('show');
             $('#register_mail_Modal').modal('handleUpdate');
         }
-        else  if ($(this).hasClass('fourth_modal')){
+        else if ($(this).hasClass('fourth_modal')) {
             $('#reset_password_modal').modal('show');
             $('#reset_password_modal').modal('handleUpdate');
         }
     });
 
-    $('.reset_password_modal .btn_send').on('click',function(){
-        var button = $(this) ,
+    $('.reset_password_modal .btn_send').on('click', function () {
+        var button = $(this),
             loader = button.siblings('.loader'),
             btn_sent = button.siblings('.btn_sent');
 
-        button.css('visibility','hidden');
+        button.css('visibility', 'hidden');
 
         setTimeout(function () {
-            loader.css('display','block');
-        },100);
+            loader.css('display', 'block');
+        }, 100);
 
         setTimeout(function () {
-            loader.css('display','none');
-            btn_sent.css('display','block');
-        },2000);
+            loader.css('display', 'none');
+            btn_sent.css('display', 'block');
+        }, 2000);
     });
-
 
 
     var inputs = $('input[type="text"],input[type="email"],input[type="password"],textarea');
@@ -96,4 +151,5 @@ $(document).ready(function () {
         }
         $(this).attr('title', title).parent('.wishlist_add').toggleClass('added', '');
     });
-});
+})
+;
